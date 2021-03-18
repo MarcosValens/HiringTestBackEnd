40010d447421be77eaff6996dfb222dadf37f3da
marcos <marcosvalens@gmail.com>
2021-03-18 01:57:55 +0100

    .geojson created

--- api/views.py
+++ api/views.py
@@ -17,33 +17,35 @@ from .serializer import HotelReviewSerializer
 
 
 class HotelInfoViewSet(viewsets.ModelViewSet):
-    geolocator = Nominatim(user_agent='http')
-    geocode_with_delay = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-    queryset = HotelInfo.objects.filter(review_qty__lte=5)
-    query = queryset.values('hotel_address')
-    df = pd.DataFrame.from_records(query)
-    df = df.loc[520:523]  # I do this line because it takes too long the geocode all results
-    df['temp'] = df['hotel_address'].apply(geocode_with_delay)
-    df['coords'] = df['temp'].apply(lambda loc: tuple(loc.point) if loc else None)
-    dfCoords = df['coords']
-    dfCoords = dfCoords.tolist()
-    dumpdf = pd.DataFrame(dfCoords, columns=['long', 'lat', 'elev']).dropna()
 
+    def dataframe_creator(query):
+        geolocator = Nominatim(user_agent='http')
+        geocode_with_delay = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+        df = pd.DataFrame.from_records(query)
+        df = df.loc[520:523]  # I do this line because it takes too long the geocode all results
+        df['temp'] = df['hotel_address'].apply(geocode_with_delay)
+        df['coords'] = df['temp'].apply(lambda loc: tuple(loc.point) if loc else None)
+        df = df['coords']
+        df = df.tolist()
+        return pd.DataFrame(df, columns=['long', 'lat', 'elev']).dropna()
 
     def data2geojson(df):
         try:
             features = []
             df.apply(lambda X: features.append(
                 geojson.Feature(geometry=geojson.Point((X["lat"],
-                                                    X["long"],
-                                                    X["elev"])),
-                            properties={"country": "Spain"})), axis=1)
+                                                        X["long"],
+                                                        X["elev"])),
+                                properties={"country": "Spain"})), axis=1)
             with open('map.geojson', 'w') as fp:
                 geojson.dump(geojson.FeatureCollection(features), fp, sort_keys=True)
         except ValueError:
             print('Out of range float values are not JSON compliant: nan')
 
-    data2geojson(dumpdf)
+    queryset = HotelInfo.objects.filter(review_qty__lte=5)
+    query = queryset.values('hotel_address')
+    df = dataframe_creator(query)
+    data2geojson(df)
     serializer_class = HotelInfoSerializer
 
 
